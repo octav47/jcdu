3ba0aa6530fd98a4975b36bc8476c1241391ea7b
tribunskiy <forty-7@yandex.ru>
2016-05-26 15:04:38 +0300

    update

--- src/o/Abstract/Abstract.js
+++ src/o/Abstract/Abstract.js
@@ -4,4 +4,6 @@ jcdu.o.Abstract.prototype.getClass = function () {
     return 'jcdu.o.Abstract';
 };
 
-jcdu.o.Abstract.prototype.method = function () {};
+jcdu.o.Abstract.prototype.method = function () {
+    return null;
+};
---
+++ src/o/List/List.js
@@ -0,0 +1,8 @@
+jcdu.o.List = function() {};
+jcdu.inherit(jcdu.o.List, jcdu.o.Collection);
+
+jcdu.o.List.prototype.get = jcdu.o.Abstract.method;
+
+jcdu.o.List.prototype.set = jcdu.o.Abstract.method;
+
+jcdu.o.List.prototype.isEmpty = jcdu.o.Abstract.method;
