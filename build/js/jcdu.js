52b9a434076af44577b54acb424b2ec13c435ff2
tribunskiy <forty-7@yandex.ru>
2016-05-26 17:06:32 +0300

    update

--- build/js/jcdu.js
+++ build/js/jcdu.js
@@ -154,7 +154,9 @@
             return 'jcdu.o.Abstract';
         };
         
-        jcdu.o.Abstract.prototype.method = function () {};
+        jcdu.o.Abstract.prototype.method = function () {
+            return null;
+        };
         
         jcdu.o.NoSuchElementException = function(message) {
             this.message = message || '';
@@ -311,6 +313,117 @@
             Iterator_.prototype.remove = function () {
                 // TODO
                 throw new jcdu.o.OperationNotSupported();
+            };
+        })();
+        
+        jcdu.o.List = function() {};
+        jcdu.inherit(jcdu.o.List, jcdu.o.Collection);
+        
+        jcdu.o.List.prototype.get = jcdu.o.Abstract.method;
+        
+        jcdu.o.List.prototype.set = jcdu.o.Abstract.method;
+        
+        jcdu.o.List.prototype.isEmpty = jcdu.o.Abstract.method;
+        (function () {
+            var Collection = jcdu.o.Collection;
+            var IndexOutOfBoundsException = jcdu.o.IndexOutOfBoundsException;
+            var NoSuchElementException = jcdu.o.NoSuchElementException;
+            var OperationNotSupported = jcdu.o.OperationNotSupported;
+        
+            jcdu.o.ArrayList = function () {
+                this.array_ = [];
+        
+                if (arguments[0] instanceof Collection) {
+                    this.addAll(arguments[0]);
+                }
+            };
+            jcdu.inherit(jcdu.o.ArrayList, jcdu.o.List);
+        
+            jcdu.o.ArrayList.prototype.getClass = function () {
+                return 'jcdu.o.ArrayList';
+            };
+        
+            jcdu.o.ArrayList.prototype.add = function (e) {
+                this.array_.push(e);
+                return true;
+            };
+        
+            jcdu.o.ArrayList.prototype.addAll = function (c) {
+                for (var i = c.iterator(); i.hasNext();) {
+                    this.add(i.next());
+                }
+                return true;
+            };
+        
+            jcdu.o.ArrayList.prototype.set = function (index, element) {
+                var oldElement = this.array_[index];
+                this.array_[index] = element;
+                return oldElement;
+            };
+        
+            jcdu.o.ArrayList.prototype.iterator = function () {
+                return new Iterator_(this);
+            };
+        
+            jcdu.o.ArrayList.prototype.get = function (index) {
+                if (index < 0 || index >= this.size()) {
+                    throw new IndexOutOfBoundsException();
+                }
+        
+                return this.array_[index];
+            };
+        
+            jcdu.o.ArrayList.prototype.isEmpty = function () {
+                return this.array_.length === 0;
+            };
+        
+            jcdu.o.ArrayList.prototype.size = function () {
+                return this.array_.length;
+            };
+        
+            jcdu.o.ArrayList.prototype.toArray = function () {
+                var array = [];
+        
+                for (var i = 0, len = this.array_.length; i < len; i++) {
+                    array.push(this.array_[i]);
+                }
+        
+                return array;
+            };
+        
+            jcdu.o.ArrayList.prototype.remove = function (o) {
+                var found = false;
+        
+                for (var i = 0, len = this.array_.length; i < len; i++) {
+                    if (this.array_[i] === o) {
+                        this.array_.splice(i, 1);
+                        found = true;
+                        break;
+                    }
+                }
+        
+                return found;
+            };
+        
+            var Iterator_ = function (arrayList) {
+                this.arrayList_ = arrayList;
+                this.position_ = 0;
+            };
+        
+            Iterator_.prototype.next = function () {
+                if (this.position_ === this.arrayList_.size()) {
+                    throw new NoSuchElementException();
+                }
+                return this.arrayList_.get(this.position_++);
+            };
+        
+            Iterator_.prototype.hasNext = function () {
+                return this.position_ < this.arrayList_.size();
+            };
+        
+            Iterator_.prototype.remove = function () {
+                // TODO
+                throw new OperationNotSupported();
             };
         })();
     })(jcdu);
